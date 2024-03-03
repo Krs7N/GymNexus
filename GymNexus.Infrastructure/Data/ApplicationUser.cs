ca96f61738d731621951b3c27a8feb5173a487fd
KMladenov <KMladenov@online.technologica.com>
2024-03-03 18:57:05 +0200

    Add initial ApplicationUser and DbContext setup

---
+++ GymNexus.Infrastructure/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GymNexus.Infrastructure.Data;
+
+public class ApplicationUser : IdentityUser
+{
+    public byte[]? ProfilePicture { get; set; }
+}
