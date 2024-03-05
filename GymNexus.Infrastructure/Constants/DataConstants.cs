b415a4db853db142179fc9dece5de06d84fc6429
KMladenov <KMladenov@online.technologica.com>
2024-03-05 13:58:21 +0200

    Add DbSets and overrided OnModelCreating

--- GymNexus.Infrastructure/Constants/DataConstants.cs
+++ GymNexus.Infrastructure/Constants/DataConstants.cs
@@ -6,30 +6,41 @@ public static class DataConstants
     public const int ProfilePictureMaxLength = 250;
 
     // Product
+    public const int ProductNameMinLength = 5;
     public const int ProductNameMaxLength = 40;
     public const int ProductImageUrlMaxLength = 250;
+    public const int ProductDescriptionMinLength = 10;
     public const int ProductDescriptionMaxLength = 500;
     public const string ProductPriceMinValue = "1.00";
     public const string ProductPriceMaxValue = "1000.00";
 
     // Store
+    public const int StoreNameMinLength = 5;
     public const int StoreNameMaxLength = 40;
+    public const int StoreDescriptionMinLength = 10;
     public const int StoreDescriptionMaxLength = 500;
 
     // Category
+    public const int CategoryNameMinLength = 5;
     public const int CategoryNameMaxLength = 50;
+    public const int CategoryDescriptionMinLength = 10;
     public const int CategoryDescriptionMaxLength = 500;
 
     // Marketplace
+    public const int MarketplaceNameMinLength = 5;
     public const int MarketplaceNameMaxLength = 50;
+    public const int MarketplaceDescriptionMinLength = 10;
     public const int MarketplaceDescriptionMaxLength = 500;
     public const int MarketplaceAddressMaxLength = 150;
 
     // Post
+    public const int PostTitleMinLength = 5;
     public const int PostTitleMaxLength = 50;
+    public const int PostDescriptionMinLength = 10;
     public const int PostContentMaxLength = 500;
     public const int PostImageUrlMaxLength = 250;
 
     // Comment
+    public const int CommentContentMinLength = 5;
     public const int CommentContentMaxLength = 250;
 }
--- GymNexus.Infrastructure/Data/ApplicationDbContext.cs
+++ GymNexus.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GymNexus.Infrastructure.Data.Configuration;
+using GymNexus.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GymNexus.Infrastructure.Data;
@@ -8,4 +10,35 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     { }
 
+    public DbSet<Marketplace> Marketplaces { get; set; } = null!;
+
+    public DbSet<Store> Stores { get; set; } = null!;
+
+    public DbSet<Category> Categories { get; set; } = null!;
+
+    public DbSet<Product> Products { get; set; } = null!;
+
+    public DbSet<Comment> Comments { get; set; } = null!;
+
+    public DbSet<Order> Orders { get; set; } = null!;
+
+    public DbSet<OrderDetail> OrdersDetails { get; set; } = null!;
+
+    public DbSet<Post> Posts { get; set; } = null!;
+
+    public DbSet<PostLike> PostsLikes { get; set; } = null!;
+
+    public DbSet<ProductLike> ProductsLikes { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new UserConfiguration());
+        builder.ApplyConfiguration(new CategoryConfiguration());
+        builder.ApplyConfiguration(new MarketplaceConfiguration());
+        builder.ApplyConfiguration(new PostConfiguration());
+        builder.ApplyConfiguration(new ProductConfiguration());
+        builder.ApplyConfiguration(new StoreConfiguration());
+
+        base.OnModelCreating(builder);
+    }
 }
