3b01863b0daf8b18e61209137141bf7ad12d8b70
KMladenov <KMladenov@online.technologica.com>
2024-03-05 13:16:01 +0200

    Add relations between entities in Post and User configurations

--- GymNexus.Infrastructure/Data/Configuration/PostConfiguration.cs
+++ GymNexus.Infrastructure/Data/Configuration/PostConfiguration.cs
@@ -9,6 +9,14 @@ public class PostConfiguration : IEntityTypeConfiguration<Post>
     public void Configure(EntityTypeBuilder<Post> builder)
     {
         builder
+            .HasOne(p => p.Creator)
+            .WithMany(u => u.Posts);
+
+        builder
+            .HasMany(p => p.Comments)
+            .WithOne(c => c.Post);
+
+        builder
             .HasQueryFilter(p => p.IsActive)
             .HasData(ConfigurationHelper.GetSeedPosts());
     }
--- GymNexus.Infrastructure/Data/Configuration/UserConfiguration.cs
+++ GymNexus.Infrastructure/Data/Configuration/UserConfiguration.cs
@@ -7,6 +7,11 @@ public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
 {
     public void Configure(EntityTypeBuilder<ApplicationUser> builder)
     {
+        builder
+            .HasMany(u => u.Comments)
+            .WithOne(c => c.Creator)
+            .OnDelete(DeleteBehavior.Restrict);
+
         builder.HasData(ConfigurationHelper.RootApplicationUser);
     }
 }
