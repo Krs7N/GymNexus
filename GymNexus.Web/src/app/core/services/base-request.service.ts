e73ba163551d3336dbd53bdcd12b55d85a64da1c
KMladenov <KMladenov@online.technologica.com>
2024-03-05 19:28:35 +0200

    Add BaseRequestService

---
+++ GymNexus.Web/src/app/core/services/base-request.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class BaseRequestService<T> {
+
+  protected httpClient: HttpClient;
+
+  constructor(protected injector: Injector) {
+    this.httpClient = injector.get<HttpClient>(HttpClient);
+  }
+
+  protected get APIPrefix(): string { return 'api/' };
+
+  protected get APIUrl(): string { return this.APIPrefix + this.getResourceUrl() };
+
+  abstract getResourceUrl(): string;
+}
