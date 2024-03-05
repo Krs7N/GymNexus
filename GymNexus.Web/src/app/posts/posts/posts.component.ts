a8152d2aae19e6d2fbb7a394da01e8a334a586a5
KMladenov <KMladenov@online.technologica.com>
2024-03-05 19:30:42 +0200

    Add Initial posts UI

--- GymNexus.Web/src/app/posts/posts/posts.component.ts
+++ GymNexus.Web/src/app/posts/posts/posts.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
+import { PostViewModel } from '../post-view-model';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit, OnDestroy {
 
+  posts: PostViewModel[] = [];
+
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
+
+  constructor(private _postsService: PostsService) {
+  }
+
+  ngOnInit(): void {
+    this._postsService.getAllPosts().pipe(takeUntil(this._unsubscribeAll)).subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next(null);
+    this._unsubscribeAll.complete();
+  }
 }
