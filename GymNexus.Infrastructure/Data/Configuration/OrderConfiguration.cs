fc025faecb0f3a4bd5dd11dfe4aa377e4ee2003e
KMladenov <KMladenov@online.technologica.com>
2024-03-05 16:05:05 +0200

    Add OrderConfiguration

---
+++ GymNexus.Infrastructure/Data/Configuration/OrderConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using GymNexus.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymNexus.Infrastructure.Data.Configuration;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder
+            .HasOne(o => o.Store)
+            .WithMany(s => s.Orders)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
