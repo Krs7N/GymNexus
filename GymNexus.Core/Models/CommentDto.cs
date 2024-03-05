164b23efd2250215345ed7a558f3758e25314d79
KMladenov <KMladenov@online.technologica.com>
2024-03-05 20:39:05 +0200

    Add CommentDto

---
+++ GymNexus.Core/Models/CommentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GymNexus.Core.Models;
+
+public class CommentDto
+{
+    public string Content { get; set; } = null!;
+
+    public DateTime CreatedOn { get; set; }
+
+    public string CreatedBy { get; set; } = null!;
+}
