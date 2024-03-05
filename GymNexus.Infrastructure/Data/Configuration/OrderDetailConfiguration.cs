5b482cc6da3433ec6c816d0be5a68f19e7dc9a1d
KMladenov <KMladenov@online.technologica.com>
2024-03-05 14:11:31 +0200

    Add OrderDetailsConfiguration to set up composite key

---
+++ GymNexus.Infrastructure/Data/Configuration/OrderDetailConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using GymNexus.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymNexus.Infrastructure.Data.Configuration;
+
+public class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+{
+    public void Configure(EntityTypeBuilder<OrderDetail> builder)
+    {
+        builder
+            .HasKey(od => new { od.OrderId, od.ProductId });
+
+        builder
+            .HasOne(od => od.Order)
+            .WithMany(o => o.OrdersDetails);
+    }
+}
