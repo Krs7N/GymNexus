3fa6b1d68abfcca145fd440056fafef454ab3141
KMladenov <KMladenov@online.technologica.com>
2024-03-05 18:29:29 +0200

    Updated routes in UI

--- GymNexus.Web/src/app/app-routing.module.ts
+++ GymNexus.Web/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', pathMatch: 'full', redirectTo: 'posts' },
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  // { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  // { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+  // { path: 'forgot-password', loadChildren: () => import('./forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
+  // { path: 'reset-password', loadChildren: () => import('./reset-password/reset-password.module').then(m => m.ResetPasswordModule) },
+  // { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
+  // { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
+  // { path: '404', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GymNexus.Web/src/app/app.module.ts
+++ GymNexus.Web/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PostsComponent } from './posts/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
