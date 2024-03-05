f21c43ededbeb6e813bc960ea0d7c63ef336665b
KMladenov <KMladenov@online.technologica.com>
2024-03-05 14:21:45 +0200

    Add ProductLikeConfiguration to set composite key

---
+++ GymNexus.Infrastructure/Data/Configuration/ProductLikeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using GymNexus.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymNexus.Infrastructure.Data.Configuration;
+
+public class ProductLikeConfiguration : IEntityTypeConfiguration<ProductLike>
+{
+    public void Configure(EntityTypeBuilder<ProductLike> builder)
+    {
+        builder
+            .HasKey(pl => new { pl.ProductId, pl.UserId });
+
+        builder
+            .HasOne(pl => pl.Product)
+            .WithMany(p => p.ProductsLikes);
+    }
+}
