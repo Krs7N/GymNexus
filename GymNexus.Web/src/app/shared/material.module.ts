8f62b2313cde184509faef41135891b008be2412
KMladenov <KMladenov@online.technologica.com>
2024-03-05 20:40:00 +0200

    Add external MaterialModule and SharedModule, with RelativeTimePipe

---
+++ GymNexus.Web/src/app/shared/material.module.ts
@@ -0,0 +1,113 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
+
+@NgModule({
+    imports: [
+        MatAutocompleteModule,
+        MatSortModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatTabsModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        MatTableModule,
+        MatStepperModule,
+        MatCardModule,
+        MatIconModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatButtonModule,
+        MatListModule,
+        MatCheckboxModule,
+        MatSnackBarModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatTreeModule,
+        MatChipsModule,
+        MatSidenavModule,
+        MatButtonToggleModule,
+        MatMenuModule,
+        MatDatepickerModule,
+        MatPaginatorModule,
+        MatToolbarModule,
+        MatBadgeModule,
+        MatProgressSpinnerModule,
+        MatBottomSheetModule,
+        MatTooltipModule,
+    ],
+    exports: [
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatSortModule,
+        MatIconModule,
+        MatTabsModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        MatTableModule,
+        MatStepperModule,
+        MatCardModule,
+        MatIconModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatButtonModule,
+        MatListModule,
+        MatCheckboxModule,
+        MatSnackBarModule,
+        MatTreeModule,
+        MatChipsModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatSidenavModule,
+        MatButtonToggleModule,
+        MatMenuModule,
+        MatDatepickerModule,
+        MatPaginatorModule,
+        MatToolbarModule,
+        MatBadgeModule,
+        MatProgressSpinnerModule,
+        MatBottomSheetModule,
+        MatSliderModule,
+        MatTooltipModule,
+    ],
+    declarations: [],
+    providers: [],
+})
+export class MaterialModule {}
