414e9ce631b54298ebe3697a52faa5dd8b1cb88e
KMladenov <KMladenov@online.technologica.com>
2024-03-05 16:49:52 +0200

    Add PostService to IoC container

--- GymNexus.API/Extensions/ServiceCollectionExtension.cs
+++ GymNexus.API/Extensions/ServiceCollectionExtension.cs
@@ -11,6 +11,7 @@ public static class ServiceCollectionExtension
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<ICloudinaryService, CloudinaryService>();
+        services.AddScoped<IPostService, PostService>();
 
         return services;
     }
---
+++ GymNexus.Core/Services/PostService.cs
@@ -0,0 +1,14 @@
+﻿using GymNexus.Core.Contracts;
+using GymNexus.Infrastructure.Data;
+
+namespace GymNexus.Core.Services;
+
+public class PostService : IPostService
+{
+    private readonly ApplicationDbContext _context;
+
+    public PostService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+}
