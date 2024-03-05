4e312a4e7d99c04d46ce8189084cf51ca0df226b
KMladenov <KMladenov@online.technologica.com>
2024-03-05 09:25:58 +0200

    Add OrderDetails entity

---
+++ GymNexus.Infrastructure/Data/Models/Order.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymNexus.Infrastructure.Data.Models;
+
+/// <summary>
+/// Order entity representation in the system. Represents the orders that are made by the users. An order can have many order details.
+/// </summary>
+[Comment("Order entity representation in the system")]
+public class Order
+{
+    /// <summary>
+    /// The unique identifier of the order. Primary Key in the database.
+    /// </summary>
+    [Key]
+    [Comment("The unique identifier of the order")]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The unique identifier of the store from which the order was made. It is required. It is a foreign key to the Store entity.
+    /// </summary>
+    [Required]
+    [Comment("The unique identifier of the store from which the order was made")]
+    public int StoreId { get; set; }
+
+    /// <summary>
+    /// The store entity representation (Navigation property) from which the order was made. It is required.
+    /// </summary>
+    [ForeignKey(nameof(StoreId))]
+    public Store Store { get; set; }
+
+    /// <summary>
+    /// The unique identifier of the user who made the order. It is required. It is a foreign key to the ApplicationUser entity.
+    /// </summary>
+    [Required]
+    [Comment("The unique identifier of the user who made the order")]
+    public string CreatedBy { get; set; } = null!;
+
+    /// <summary>
+    /// The user entity representation (Navigation property) who made the order. It is required.
+    /// </summary>
+    [ForeignKey(nameof(CreatedBy))]
+    public ApplicationUser Creator { get; set; }
+
+    /// <summary>
+    /// The date and time when the order was made. Set on creation of the order. It is required.
+    /// </summary>
+    [Required]
+    [Comment("The date and time when the order was made. Set on creation of the order")]
+    public DateTime CreatedOn { get; set; }
+
+    /// <summary>
+    /// The price for a unit in the order. It is required. It is a decimal number with 4 digits in total, and 2 of them are after the decimal point.
+    /// </summary>
+    [Required]
+    [Comment("The price for a unit in the order")]
+    [Column(TypeName = "decimal(4,2)")]
+    public decimal UnitPrice { get; set; }
+
+    /// <summary>
+    /// The quantity of the products in the order. It is required. Based on this quantity and the unit price, the total price is calculated.
+    /// </summary>
+    [Required]
+    [Comment("The quantity of the products in the order")]
+    public int Quantity { get; set; }
+
+    /// <summary>
+    /// The total price of the order. It is required. It is a decimal number with 9 digits in total, and 2 of them are after the decimal point.
+    /// </summary>
+    [Required]
+    [Comment("The total price of the order")]
+    [Column(TypeName = "decimal(9,2)")]
+    public decimal TotalPrice { get; set; }
+
+    /// <summary>
+    /// The status of the order. It is required. It represents the current status of the order. It can be "In Progress" or "Completed".
+    /// </summary>
+    [Required]
+    [Comment("The status of the order")]
+    public string Status { get; set; } = null!;
+
+    /// <summary>
+    /// The payment method for the order. It is required. It represents the method that the user used to pay for the order. It can be "In cash", "By Card" or "Paypal"
+    /// </summary>
+    [Required]
+    [Comment("The payment method for the order")]
+    public string PaymentMethod { get; set; } = null!;
+
+    /// <summary>
+    /// The status of the order. Represents if it is active or not. Set to true by default.
+    /// </summary>
+    [DefaultValue(true)]
+    [Comment("The status of the order. Represents if it is active or not. Set to true by default")]
+    public bool IsActive { get; set; }
+
+    /// <summary>
+    /// The collection of all order details that the order has. It is a navigation property. Gets the other products that could be in this order
+    /// </summary>
+    public virtual IEnumerable<OrderDetails> OrdersDetails { get; set; } = new List<OrderDetails>();
+}
---
+++ GymNexus.Infrastructure/Data/Models/OrderDetail.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymNexus.Infrastructure.Data.Models;
+
+/// <summary>
+/// The order details of an order with the different products entity representation in the system. An order detail is a product that is included in an order.
+/// </summary>
+[Comment("The order details of an order with the different products entity representation in the system")]
+public class OrderDetail
+{
+    /// <summary>
+    /// The unique identifier of the order detail. Part of composite key in the database.
+    /// </summary>
+    [Required]
+    [Comment("The unique identifier of the order")]
+    public int OrderId { get; set; }
+
+    /// <summary>
+    /// The order entity representation (Navigation property) to which the order detail is related. It is required. It is a foreign key to the Order entity.
+    /// </summary>
+    [ForeignKey(nameof(OrderId))]
+    public Order Order { get; set; } = null!;
+
+    /// <summary>
+    /// The unique identifier of the product that is included in the order. Part of composite key in the database.
+    /// </summary>
+    [Required]
+    [Comment("The unique identifier of the product that is included in the order")]
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// The product entity representation (Navigation property) that is included in the order. It is required. It is a foreign key to the Product entity.
+    /// </summary>
+    [ForeignKey(nameof(ProductId))]
+    public Product Product { get; set; } = null!;
+}
