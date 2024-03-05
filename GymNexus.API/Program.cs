fe17b949f9a80e3d7ab3d3c5cf71905c2ce9ee1b
KMladenov <KMladenov@online.technologica.com>
2024-03-05 16:47:31 +0200

    Enabled CORS

--- GymNexus.API/Program.cs
+++ GymNexus.API/Program.cs
@@ -21,6 +21,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
 app.UseStaticFiles();
 
 app.UseAuthentication();
