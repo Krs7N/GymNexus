a807786b89e505ac5e89752b0dba72ee134db854
KMladenov <KMladenov@online.technologica.com>
2024-03-05 10:57:37 +0200

    Add Post and Category configurations

---
+++ GymNexus.Infrastructure/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using GymNexus.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymNexus.Infrastructure.Data.Configuration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder
+            .HasQueryFilter(c => c.IsActive)
+            .HasData(ConfigurationHelper.GetSeedCategories());
+    }
+}
---
+++ GymNexus.Infrastructure/Data/Configuration/PostConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using GymNexus.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymNexus.Infrastructure.Data.Configuration;
+
+public class PostConfiguration : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        builder
+            .HasQueryFilter(p => p.IsActive)
+            .HasData(ConfigurationHelper.GetSeedPosts());
+    }
+}
