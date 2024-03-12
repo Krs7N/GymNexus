d5e5f08c23f1c9bc163b87569c2b54b79e963c78
KMladenov <KMladenov@online.technologica.com>
2024-03-12 18:02:26 +0200

    Add facebook config constants

---
+++ GymNexus.Core/Utils/FacebookAuthConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace GymNexus.Core.Utils;
+
+public static class FacebookAuthConfig
+{
+    public static string AppId => "Authentication:Facebook:AppId";
+
+    public static string AppSecret => "Authentication:Facebook:AppSecret";
+}
