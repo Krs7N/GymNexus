7cfafd9fedd687c406e2dd3deb0a3c24f7b7c4ef
KMladenov <KMladenov@online.technologica.com>
2024-03-12 10:47:27 +0200

    Add confirm dialog

--- GymNexus.Web/src/app/shared/shared.module.ts
+++ GymNexus.Web/src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RelativeTimePipe } from './relative-time.pipe';
 import { CloudinaryModule } from '@cloudinary/ng';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-    declarations: [RelativeTimePipe],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, CloudinaryModule],
-    exports: [CommonModule, FormsModule, ReactiveFormsModule, RelativeTimePipe, CloudinaryModule],
+    declarations: [RelativeTimePipe, ConfirmDialogComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, CloudinaryModule, MaterialModule],
+    exports: [CommonModule, FormsModule, ReactiveFormsModule, RelativeTimePipe, CloudinaryModule, ConfirmDialogComponent],
 })
 export class SharedModule {}
