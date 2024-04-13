2a80693e212f15cf14ff550a784ed3f1ee239d2a
KMladenov <KMladenov@online.technologica.com>
2024-04-13 15:57:29 +0300

    Add StoreService tests and ProfileService tests

---
+++ GymNexus.Tests/ProfileServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using GymNexus.Core.Contracts;
+using GymNexus.Core.Models;
+using GymNexus.Core.Services;
+using GymNexus.Infrastructure.Data;
+using GymNexus.Infrastructure.Data.Models;
+
+namespace GymNexus.Tests;
+
+[TestFixture]
+public class ProfileServiceTests : TestBase
+{
+    private IProfileService _profileService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _profileService = new ProfileService(_userManagerMock.Object, _context);
+    }
+
+    [Test]
+    public async Task UpdateProfileAsyncReturnsProfileUpdateResponseDto()
+    {
+        var profileUpdateDto = new ProfileUpdateDto()
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = User.Email,
+            ImageUrl = "https://www.example.com/image.jpg"
+        };
+
+        var result = await _profileService.UpdateProfileAsync(profileUpdateDto, User);
+
+        Assert.NotNull(result);
+        Assert.That(result.FirstName, Is.EqualTo(profileUpdateDto.FirstName));
+        Assert.That(result.LastName, Is.EqualTo(profileUpdateDto.LastName));
+        Assert.That(result.Email, Is.EqualTo(profileUpdateDto.Email));
+        Assert.That(result.ImageUrl, Is.EqualTo(profileUpdateDto.ImageUrl));
+    }
+
+    [Test]
+    public async Task GetUserStoresAsyncReturnsUserStores()
+    {
+        var stores = new List<Store>
+        {
+            new Store() { Id = 10, Name = "Store 1", OwnerId = User.Id, Description = "Test Description 112323", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now},
+            new Store() { Id = 11, Name = "Store 2", OwnerId = User.Id, Description = "Test Longer Description", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now}
+        };
+
+        _context.Stores.AddRange(stores);
+        await _context.SaveChangesAsync();
+
+        var result = await _profileService.GetUserStoresAsync(User.Id);
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(2));
+    }
+
+    [Test]
+    public async Task GetUserStoresAsyncReturnsEmptyListIfNoStoresFound()
+    {
+        var result = await _profileService.GetUserStoresAsync(User.Id);
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task GetUserStoresAsyncReturnsEmptyListIfUserDoesNotExist()
+    {
+        var stores = new List<Store>
+        {
+            new Store() { Id = 10, Name = "Store 1", OwnerId = User.Id, Description = "Test Description 112323", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now},
+            new Store() { Id = 11, Name = "Store 2", OwnerId = User.Id, Description = "Test Longer Description", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now}
+        };
+
+        _context.Stores.AddRange(stores);
+        await _context.SaveChangesAsync();
+
+        var result = await _profileService.GetUserStoresAsync("non-existing-user-id");
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(0));
+    }
+}
---
+++ GymNexus.Tests/StoreServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using GymNexus.Core.Contracts;
+using GymNexus.Core.Models;
+using GymNexus.Core.Services;
+using GymNexus.Infrastructure.Data.Models;
+using static GymNexus.Infrastructure.Constants.DataConstants;
+
+namespace GymNexus.Tests;
+
+[TestFixture]
+public class StoreServiceTests : TestBase
+{
+    private IStoreService _storeService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _storeService = new StoreService(_context);
+    }
+
+    [Test]
+    public async Task GetAllStoresAsyncReturnsAllStores()
+    {
+        var stores = new List<Store>
+        {
+            new Store() { Id = 10, Name = "Store 1", OwnerId = User.Id, Description = "Test Description 112323", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now},
+            new Store() { Id = 11,Name = "Store 2", OwnerId = User.Id, Description = "Test Longer Description", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now}
+        };
+
+        _context.Stores.AddRange(stores);
+        await _context.SaveChangesAsync();
+
+        var result = await _storeService.GetAllStoresAsync();
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(3));
+    }
+
+    [Test]
+    public async Task GetStoresByMarketplaceIdAsyncReturnsStoresByMarketplaceId()
+    {
+        var stores = new List<Store>
+        {
+            new Store() { Id = 10, Name = "Store 1", OwnerId = User.Id, Description = "Test Description 112323", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now, MarketplaceId = 4},
+            new Store() { Id = 11,Name = "Store 2", OwnerId = User.Id, Description = "Test Longer Description", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now, MarketplaceId = 4}
+        };
+
+        _context.Stores.AddRange(stores);
+        await _context.SaveChangesAsync();
+
+        var result = await _storeService.GetStoresByMarketplaceIdAsync(4);
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(2));
+    }
+
+    [Test]
+    public async Task GetStoresByMarketplaceIdAsyncReturnsEmptyListIfNoStoresFound()
+    {
+        var result = await _storeService.GetStoresByMarketplaceIdAsync(4);
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task GetStoresByMarketplaceIdAsyncReturnsEmptyListIfMarketplaceIdDoesNotExist()
+    {
+        var stores = new List<Store>
+        {
+            new Store() { Id = 10, Name = "Store 1", OwnerId = User.Id, Description = "Test Description 112323", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now, MarketplaceId = 4},
+            new Store() { Id = 11,Name = "Store 2", OwnerId = User.Id, Description = "Test Longer Description", RatingsCount = 2, AverageRating = 2.5m, CreatedOn = DateTime.Now, MarketplaceId = 4}
+        };
+
+        _context.Stores.AddRange(stores);
+        await _context.SaveChangesAsync();
+
+        var result = await _storeService.GetStoresByMarketplaceIdAsync(5);
+
+        Assert.NotNull(result);
+        Assert.That(result.Count(), Is.EqualTo(0));
+    }
+}
